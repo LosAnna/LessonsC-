dff7bad432b824c0d18122721281debc1e5d970b
losanna <79831340359@yandex.ru>
2022-08-23 23:37:23 +0700

    Task 2

---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("{0} Наибольшее число", number1);
+}
+else
+{
+    Console.WriteLine("{0} Наибольшее число", number2);
+}
+Console.WriteLine("Наибольшее число");
