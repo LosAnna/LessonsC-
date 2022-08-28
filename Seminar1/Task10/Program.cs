059acb0ca9120f6cebc3c9d3a0a49c54a21cfb67
losanna <79831340359@yandex.ru>
2022-08-28 23:24:56 +0700

    Task 10

---
+++ Seminar1/Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число от 100 до 999: ");
+int num = int.Parse(Console.ReadLine());
+int result = ( num % 100) / 10;
+Console.WriteLine($"{num}, result: {result}");
+
