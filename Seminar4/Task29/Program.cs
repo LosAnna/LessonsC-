f7361c8fbc33463e808aa66249debeb1d16297a4
losanna <79831340359@yandex.ru>
2022-09-06 01:24:15 +0700

    Task 29

---
+++ Seminar4/Task29/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая задаёт массив из N элементов, заполненных случайнми числами из [a, b) и выводит их на экран.
+
+int[] InitArray(int size, int numA, int numB)
+{
+int[] res = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+res[i] = new Random().Next(numA, numB);
+}
+return res;
+}
+
+int[] ReadArray(int[] array)
+{
+int[] res = new int[2];
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] > 0)
+{
+res[0] += array[i];
+}
+else
+{
+res[1] += array[i];
+}
+
+}
+
+return res;
+}
+
+Console.WriteLine("Введите число, обозначающее размер массива: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число A: ");
+ int numberA  = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB  = int.Parse(Console.ReadLine());
+
+int[] myArray = InitArray(N, numberA, numberB );
+
+Console.WriteLine(String.Join(" ", myArray));
