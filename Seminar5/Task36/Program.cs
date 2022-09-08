840ccce68e8c82db350d8104c958e79458c18ed8
losanna <79831340359@yandex.ru>
2022-09-09 00:03:08 +0700

    Task 36

---
+++ Seminar5/Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+int SumElements(int[] array)
+{
+    int result = 0;
+    for (int i = array.Length - 1 - (array.Length & 1); i >= 0; i -= 2)
+    {
+        result = result + array[i];
+    }
+    return result;    
+}
+
+int[] myArray = GetArray(10, 10, 100);
+Console.WriteLine(String.Join(" ", myArray));
+
+int result = SumElements(myArray);
+Console.WriteLine($"{(result)}");
