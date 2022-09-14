450e90be675dc67e930241f3868afbc66bba0937
losanna <79831340359@yandex.ru>
2022-09-15 01:34:41 +0700

    Task 41

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,60 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] StringNumbers(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ' ')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+int BolsheNull(int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            result++;
+        }    
+    }
+    return result;
+}
+
+Console.Write("Введите числа через пробел: ");
+int[] numb = StringNumbers(Console.ReadLine());
+
+int result = BolsheNull(numb);
+Console.WriteLine($"Количество чисел больше 0 = {result}");
+
+
