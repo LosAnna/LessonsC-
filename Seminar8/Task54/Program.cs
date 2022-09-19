0fb4172789b5de4e2121f7e47e7adea80eae6b66
losanna <79831340359@yandex.ru>
2022-09-19 23:20:25 +0700

    Task 54

---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10 + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void SortRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int t = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = t;
+                }
+            }
+        }
+    }
+}
+
+int[,] myArray = GetArray(5, 5);
+PrintArray(myArray);
+Console.WriteLine();
+SortRows(myArray);
+PrintArray(myArray);
