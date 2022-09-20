6bbe10fb80c0a7613fedbf85ab7e296e677d059a
losanna <79831340359@yandex.ru>
2022-09-20 16:49:56 +0700

    Task 60, 62

---
+++ Seminar8/Task60/Program.cs
@@ -0,0 +1,64 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+void GetArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                while (array[i, j, k] == 0)
+                {
+                    int number = new Random().Next(10, 99 + 1);
+
+                    if (NumberInArray(array, number) == false)
+                    {
+                        array[i, j, k] = number;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+bool NumberInArray(int[,,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == number) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+void PrintArrayWithIdex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k]);
+                Console.Write("({0},{1},{2})\t", i, j, k);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[,,] testArray = new int[2, 2, 2];
+GetArray(testArray);
+PrintArrayWithIdex(testArray);
---
+++ Seminar8/Task62/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+void GetArraySpiral(int[,] array)
+{
+    int positionI = 0;
+    int positionJ = 0;
+    bool newSpiral = true;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[positionI, positionJ] = i + 1;
+
+        if (positionJ + 1 < array.GetLength(1) && newSpiral && array[positionI, positionJ + 1] == 0)
+        {
+            positionJ++;
+            continue;
+        }
+        else
+        {
+            newSpiral = false;
+        }
+
+        if (positionI + 1 < array.GetLength(0) && array[positionI + 1, positionJ] == 0)
+        {
+            positionI++;
+            continue;
+        }
+
+        if (positionJ - 1 >= 0 && array[positionI, positionJ - 1] == 0)
+        {
+            positionJ--;
+            continue;
+        }
+
+        if (positionI - 1 >= 0 && array[positionI - 1, positionJ] == 0)
+        {
+            positionI--;
+            continue;
+        }
+        else
+        {
+            positionJ++;
+            newSpiral = true;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] testArray = new int[4, 4];
+GetArraySpiral(testArray);
+PrintArray(testArray);
